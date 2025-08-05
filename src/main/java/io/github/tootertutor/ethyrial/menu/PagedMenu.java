6f908e74f45cc3d478e151f1e14cbdb2d55a5c3b
TooterTutor <tootertutor@proton.me>
2025-08-04 19:02:35 -0600

    Added Pagination support

--- src/main/java/io/github/tootertutor/ethyrial/menu/PagedMenu.java
+++ src/main/java/io/github/tootertutor/ethyrial/menu/PagedMenu.java
@@ -3,9 +3,11 @@ package io.github.tootertutor.ethyrial.menu;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.bukkit.Material;
+import org.bukkit.DyeColor;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
+import io.github.tootertutor.ethyrial.Ethyrial;
 import net.kyori.adventure.text.Component;
 
 /**
@@ -15,62 +17,81 @@ public abstract class PagedMenu<T> extends Menu {
 
     protected int page = 0;
     protected List<T> items = new ArrayList<>();
-    protected int itemsPerPage = 10; // default value
+    protected int itemsPerPage = 21;
+    protected DyeColor themeColor = DyeColor.BLACK;
+    protected boolean showBackButton = true;
+    protected Menu backTarget;
 
     public PagedMenu(Player player, int size, Component title) {
         super(player, size, title);
     }
 
-    /**
-     * Set the items to display in the menu.
-     * 
-     * @param items the list of items
-     */
     public void setItems(List<T> items) {
         this.items = items;
     }
 
-    /**
-     * Set the number of items to display per page.
-     * 
-     * @param itemsPerPage number of items per page
-     */
     public void setItemsPerPage(int itemsPerPage) {
         this.itemsPerPage = itemsPerPage;
     }
 
-    /**
-     * Called to render one page of items.
-     *
-     * @param startIndex start index of items for this page
-     */
+    public void setThemeColor(DyeColor color) {
+        this.themeColor = color;
+    }
+
+    public void setBackTarget(Menu backTarget) {
+        this.backTarget = backTarget;
+        this.showBackButton = true;
+    }
+
     protected abstract void renderPage(int startIndex);
 
     @Override
     public void render() {
+        MenuUtils.applyBorder(getInventory(), themeColor, Ethyrial.getInstance(), MenuUtils.BorderStyle.TOP,
+                MenuUtils.BorderStyle.BOTTOM);
+
         int start = page * itemsPerPage;
         renderPage(start);
 
-        if (page > 0) {
-            setItem(inventory.getSize() - 7, MenuUtils.createItem(Material.ARROW, Component.text("Previous Page")),
-                    e -> {
-                        page--;
-                        MenuManager.open(player, this);
-                    });
-        }
-
-        if ((page + 1) * itemsPerPage < items.size()) {
-            setItem(inventory.getSize() - 1, MenuUtils.createItem(Material.ARROW, Component.text("Next Page")), e -> {
-                page++;
-                MenuManager.open(player, this);
-            });
-        }
+        int maxPage = (int) Math.ceil((double) items.size() / itemsPerPage);
+        MenuUtils.setPaginationControls(this, page, maxPage, themeColor, backTarget);
     }
 
-    /**
-     * Refresh the menu by rendering and opening it again.
-     */
     public void refresh() {
         MenuManager.open(player, this);
     }
+
+    public void nextPage() {
+        if ((page + 1) * itemsPerPage < items.size()) {
+            page++;
+            refresh();
+        }
+    }
+
+    public void previousPage() {
+        if (page > 0) {
+            page--;
+            refresh();
+        }
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getMaxPage() {
+        return (int) Math.ceil((double) items.size() / itemsPerPage);
+    }
+
+    protected void fillRow(int row, ItemStack item) {
+        int startSlot = row * 9;
+        for (int i = 0; i < 9; i++) {
+            setItem(startSlot + i, item, null); // optional: set click handler null
+        }
+    }
+
+    protected void clearItems() {
+        inventory.clear(); // or loop setItem(i, null, null) if you need click map reset
+    }
+
 }
